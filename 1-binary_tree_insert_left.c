56f0c75844d00f0037df9d1bc1936050f11eee86
Sufi-to <3thomas225@gmail.com>
2024-03-27 01:57:18 +0400

    let the games begin

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to insert
+ * the left-child in
+ * @value: value to put in the new node
+ * Return: pointer to the new node or NULL
+*/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+}
