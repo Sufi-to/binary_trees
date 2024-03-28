701a8b52224349996a6ce0755ae0fea8b0ddda26
Sufi-to <3thomas225@gmail.com>
2024-03-28 04:21:49 +0400

    let's go

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,5 +6,24 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	
+	if (tree != NULL)
+	{
+		size_t lst = 0;
+		size_t rst = 0;
+
+		if (tree->left)
+			lst = 1 + binary_tree_height(tree->left);
+
+		if (tree->right)
+			rst = 1 + binary_tree_height(tree->right);
+
+		if (lst > rst)
+		{
+			return (lst);
+		}
+		else
+		{
+			return (rst);
+		}
+	}
 }
